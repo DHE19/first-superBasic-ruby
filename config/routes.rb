542a99643a156be22e8d9a5af575cfb97790a93d
dhe19 <info.die@gmail.com>
2023-04-26 20:51:38 -0600

    Added a homepage and about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  #define the homepage as root
+  root  'pages#home'
+  # define the path "about" to show the file saved on 'pages/about'
+  get 'about' => 'pages#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
