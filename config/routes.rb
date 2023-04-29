b15b8e7217841492a6140895dd4902c146a04c14
dhe19 <info.die@gmail.com>
2023-04-28 23:53:31 -0600

    bootstrap added

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root  'pages#home'
   # define the path "about" to show the file saved on 'pages/about'
   get 'about' => 'pages#about'
+
+  get 'contact' => 'pages#contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
